be8f685ac17daccce0297fc573f3245522d12c4e
akshith-3 <gakshithrao1663@gmail.com>
2023-01-13 15:30:02 +0530

    initial

---
+++ __tests__/new.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable no-undef */
+describe("First test suite", () => {
+  test("First case", () => {
+    expect(true).toBe(true);
+  });
+});
