4c8767846b2535c558e2a4ffe2333fe10523ac6f
akshith-3 <gakshithrao1663@gmail.com>
2023-01-13 19:33:24 +0530

    entry page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -9,6 +10,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", async function (request, response) {
   response.render("index");
+});
+
+app.get("/signup", function (request, response) {
+  response.render("signup");
+});
+
+app.get("/signin", function (request, response) {
+  response.render("signin");
 });
 
 module.exports = app;
