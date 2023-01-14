b2d7a21b78592193dbe99f6c137dd108b04b9f3e
akshith-3 <gakshithrao1663@gmail.com>
2023-01-14 15:17:15 +0530

    Initial_1

--- __tests__/new.js
+++ __tests__/new.js
@@ -1,8 +1,73 @@
-/* eslint-disable indent */
-/* eslint-disable eol-last */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-describe("First test suite", () => {
-  test("First case", () => {
-    expect(true).toBe(true);
+const request = require("supertest");
+const cheerio = require("cheerio");
+//const csrf = require("tiny-csrf")
+const db = require("../models/index");
+const app = require("../app");
+let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+describe("My-Voting-App", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  test("Sign up for first user", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/user").send({
+      fName: "Test",
+      lName: "User A",
+      email: "user.a@test.com",
+      passw: "12345678",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Sign Out for first user", async () => {
+    let res = await agent.get("/elections");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.get("/signout").send({
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("Sign up for second user", async () => {
+    let response = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(response);
+    response = await agent.post("/user").send({
+      fName: "Test",
+      lName: "User B",
+      email: "user.b@test.com",
+      passw: "12345678",
+      _csrf: csrfToken,
+    });
+    expect(response.statusCode).toBe(302);
+  });
+
+  test("Sign Out for second user", async () => {
+    let response = await agent.get("/election");
+    const csrfToken = extractCsrfToken(response);
+    response = await agent.get("/signout").send({
+      _csrf: csrfToken,
+    });
+    expect(response.statusCode).toBe(302);
   });
 });
--- app.js
+++ app.js
@@ -2,36 +2,222 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
-const session = require("express-session");
-const passport = require("passport");
-const connectEnsureLogin = require("connect-ensure-login");
+var csrf = require("tiny-csrf");
+const flash = require("connect-flash");
+const { elec, admin } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+var cookieParser = require("cookie-parser");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+const passport = require("passport");
+const session = require("express-session");
+const connectEnsureLogin = require("connect-ensure-login");
+app.use(cookieParser("ssh! some secret string"));
 app.set("view engine", "ejs");
+const path = require("path");
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+
+const LocalStrategy = require("passport-local");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 
+app.use(flash());
+
+app.use(
+  session({
+    secret: "my-secret-super-key-21728172615261562",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "passw",
+    },
+    (username, password, done) => {
+      admin
+        .findOne({ where: { email: username } })
+        .then(async function (user) {
+          const result = await bcrypt.compare(password, user.passw);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch((error) => {
+          return done(null, false, { message: "Invalid Email-Id" });
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  admin
+    .findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
 app.get("/", async function (request, response) {
-  response.render("index");
+  response.render("index", {
+    csrfToken: request.csrfToken(),
+  });
 });
 
-app.get("/signup", function (request, response) {
-  response.render("signup");
+app.get("/signout", (request, response, next) => {
+  request.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    response.redirect("/");
+  });
 });
 
-app.get("/signin", function (request, response) {
-  response.render("signin");
+app.post("/user", async (request, response) => {
+  const hashedpwd = await bcrypt.hash(request.body.passw, saltRounds);
+  const fName = request.body.fName;
+  const lName = request.body.lName;
+  const email = request.body.email;
+  const pwd = request.body.passw;
+  if (!fName) {
+    request.flash("error", "Please make sure you enter first name");
+    return response.redirect("/signup");
+  }
+  if (!lName) {
+    request.flash("error", "Please make sure you enter last name");
+    return response.redirect("/signup");
+  }
+  if (!email) {
+    request.flash("error", "Please make sure you enter Email-ID");
+    return response.redirect("/signup");
+  }
+  if (!pwd) {
+    request.flash("error", "Please make sure you enter password");
+    return response.redirect("/signup");
+  }
+  console.log(hashedpwd);
+  try {
+    const admin1 = await admin.create({
+      fName: request.body.fName,
+      lName: request.body.lName,
+      email: request.body.email,
+      passw: hashedpwd,
+    });
+    request.login(admin1, (err) => {
+      if (err) {
+        response.redirect("/signin");
+      }
+      response.redirect("/election");
+    });
+  } catch (error) {
+    console.log(error);
+    request.flash("error", error.message);
+    return response.redirect("/signup");
+  }
 });
 
 app.post(
   "/session",
   passport.authenticate("local", {
     failureRedirect: "/signin",
+    failureFlash: true,
   }),
   function (request, response) {
+    console.log(request.user);
     response.redirect("/election");
   }
 );
+
+app.get(
+  "/election",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const loggedInUser = request.user.id;
+    const newelections1 = await elec.allelec(loggedInUser);
+    if (request.accepts("html")) {
+      response.render("election", {
+        newelections1,
+        csrfToken: request.csrfToken(),
+      });
+    } else {
+      response.json({
+        newelections1,
+      });
+    }
+  }
+);
+
+app.get(
+  "/election/:id",
+  connectEnsureLogin.ensureLoggedIn(),
+  async function (request, response) {
+    try {
+      const Election1 = await elec.findByPk(request.params.id);
+      return response.json(Election1);
+    } catch (error) {
+      console.log(error);
+      return response.status(422).json(error);
+    }
+  }
+);
+
+app.post(
+  "/election/create",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const title1 = request.body.name;
+    const loggedInUser = request.user.id;
+    try {
+      const Election1 = await elec.createelec({
+        name: title1,
+        adminId: loggedInUser,
+      });
+      response.redirect("/election");
+    } catch (error) {
+      console.log(error);
+      return response.status(422).json(error);
+    }
+  }
+);
+
+app.get("/create", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
+  res.render("create", { csrfToken: req.csrfToken() });
+});
+
+app.get("/signup", function (req, res) {
+  res.render("signup", { csrfToken: req.csrfToken() });
+});
+
+app.get("/signin", function (req, res) {
+  res.render("signin", { csrfToken: req.csrfToken() });
+});
 
 module.exports = app;
